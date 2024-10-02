2254a0df88f751afead3c72dbf0c932e3b9150a7
ttooaa2020 <sashin9622@gmail.com>
2024-10-02 13:58:03 +0900

    구현 완료

--- 01-GNB/01/js/script.js
+++ 01-GNB/01/js/script.js
@@ -1,15 +1,20 @@
 $(function () {
     // 대상을 변수에 저장
+    // 대상을 변수에 담을때만 $사용 하는게 좋긴함
     const $menu = $(".gnb > li");
     const $submenu = $(".submenu");
+    const duration = 300; // 0.3초
 
     // 메뉴에 마우스가 들어왔을 때
-    $menu.on("mouseeter", function () {
-        $submenu.slideDown();
+    $menu.on("mouseenter", function () {
+        //.stop() : 현재 진행 중인 애니메이션 즉시 중지
+        $(this).find($submenu).stop().slideDown(duration);
+        $(this).addClass("on");
     });
 
     // 메뉴에 마우스가 나갔을 때
-    $submenu.on("mouseeter", function () {
-        $submenu.slideDown();
+    $menu.on("mouseleave", function () {
+        $submenu.slideUp(duration);
+        $(this).removeClass("on");
     });
 });
