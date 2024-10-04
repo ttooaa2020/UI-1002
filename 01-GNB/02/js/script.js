35a71631e1cea98b213841d5f2f1d8ab8f2da567
ttooaa2020 <sashin9622@gmail.com>
2024-10-04 10:53:31 +0900

    삼항조건문으로 수정

--- 01-GNB/02/js/script.js
+++ 01-GNB/02/js/script.js
@@ -22,12 +22,18 @@ $(function () {
     $window.on("wheel", function (e) {
         // console.log(e);
 
-        if (e.originalEvent.wheelDelta > 0) {
+        /* if (e.originalEvent.wheelDelta > 0) {
             // 휠을 올렸을때
             $header.removeClass("hide");
         } else {
             // 휠을 내렸을때
             $header.addClass("hide");
-        }
+        } */
+
+        // 삼항조건연산자
+        // 조건 ? 참일때 동작 : 거짓일때 동작
+        // 위 내용을 간단히 함
+
+        e.originalEvent.wheelDelta > 0 ? $header.removeClass("hide") : $header.addClass("hide");
     });
 });
