2dfb35bc425aa7a88e15f3945adf411d663426e7
ttooaa2020 <sashin9622@gmail.com>
2024-10-04 10:46:55 +0900

    최종완성

--- 01-GNB/02/js/script.js
+++ 01-GNB/02/js/script.js
@@ -1,4 +1,5 @@
 $(function () {
+    const $window = $(window);
     const $header = $("header");
     const $menu = $(".gnb>li");
     const $submenu = $(".submenu");
@@ -15,5 +16,18 @@ $(function () {
         $(this).removeClass("on");
         $header.removeClass("active");
         $submenu.stop().slideUp(duration);
+    });
+
+    // 마우스 휠을 조작했을때
+    $window.on("wheel", function (e) {
+        // console.log(e);
+
+        if (e.originalEvent.wheelDelta > 0) {
+            // 휠을 올렸을때
+            $header.removeClass("hide");
+        } else {
+            // 휠을 내렸을때
+            $header.addClass("hide");
+        }
     });
 });
