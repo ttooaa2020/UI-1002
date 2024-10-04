57c7f0ace47ca2256093b20d7d8a80ecae24c302
ttooaa2020 <sashin9622@gmail.com>
2024-10-04 09:57:57 +0900

    GNB 배경추가 및 동작 완성

--- 01-GNB/02/js/script.js
+++ 01-GNB/02/js/script.js
@@ -1,14 +1,19 @@
 $(function () {
+    const $header = $("header");
     const $menu = $(".gnb>li");
     const $submenu = $(".submenu");
     const duration = 300;
 
     // gnb
     $menu.on("mouseenter", function () {
+        $(this).addClass("on");
+        $header.addClass("active");
         $submenu.stop().slideDown(duration);
     });
 
     $menu.on("mouseleave", function () {
+        $(this).removeClass("on");
+        $header.removeClass("active");
         $submenu.stop().slideUp(duration);
     });
 });
