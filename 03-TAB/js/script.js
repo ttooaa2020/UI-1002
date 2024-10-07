f9ff809231f8e4cdde442b8b68c374d55e24e7ea
ttooaa2020 <sashin9622@gmail.com>
2024-10-07 11:47:15 +0900

    TAB 구현

--- 03-TAB/js/script.js
+++ 03-TAB/js/script.js
@@ -4,4 +4,29 @@ $(function () {
     const $tabCon = $(".tab-con-item");
 
     console.log($tabMenu, $tabCon);
+
+    // 처음 세팅
+    $tabMenu.removeClass("on");
+    $tabMenu.eq(0).addClass("on");
+
+    $tabCon.hide();
+    $tabCon.eq(0).show();
+
+    // 탭메뉴를 클릭 했을때
+    $tabMenu.on("click", function (e) {
+        // a의 기본 동작막기
+        e.preventDefault();
+
+        // 선택한 탭메뉴의 인덱스 구하기
+        const tabIdx = $(this).index();
+        console.log(tabIdx);
+
+        // 탭메뉴 활성화
+        $tabMenu.removeClass("on");
+        $tabMenu.eq(tabIdx).addClass("on");
+
+        // 인덱스에 해당하는 $tabCon 보이기
+        $tabCon.hide();
+        $tabCon.eq(tabIdx).show();
+    });
 });
