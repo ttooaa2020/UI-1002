d7f141a80b6d2acee307c05a96920a7439eebb12
ttooaa2020 <sashin9622@gmail.com>
2024-10-07 12:00:31 +0900

    공통의 동작을 함수로 구현

--- 03-TAB/js/script.js
+++ 03-TAB/js/script.js
@@ -5,12 +5,14 @@ $(function () {
 
     console.log($tabMenu, $tabCon);
 
-    // 처음 세팅
-    $tabMenu.removeClass("on");
-    $tabMenu.eq(0).addClass("on");
+    /*     // 처음 세팅
+        $tabMenu.removeClass("on");
+        $tabMenu.eq(0).addClass("on");
+    
+        $tabCon.hide();
+        $tabCon.eq(0).show(); */
 
-    $tabCon.hide();
-    $tabCon.eq(0).show();
+    tabAction(0); /* 처음에 보여지고 싶은것 선정 */
 
     // 탭메뉴를 클릭 했을때
     $tabMenu.on("click", function (e) {
@@ -21,12 +23,17 @@ $(function () {
         const tabIdx = $(this).index();
         console.log(tabIdx);
 
+        tabAction(tabIdx);
+    });
+
+    // 공통의 동작을 함수로 정의
+    function tabAction(index) {
         // 탭메뉴 활성화
         $tabMenu.removeClass("on");
-        $tabMenu.eq(tabIdx).addClass("on");
+        $tabMenu.eq(index).addClass("on");
 
         // 인덱스에 해당하는 $tabCon 보이기
         $tabCon.hide();
-        $tabCon.eq(tabIdx).show();
-    });
+        $tabCon.eq(index).show();
+    }
 });
