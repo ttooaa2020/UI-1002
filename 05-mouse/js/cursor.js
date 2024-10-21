4a9ccf7045bacaf5b427a5676bb76d12519c0cff
ttooaa2020 <sashin9622@gmail.com>
2024-10-21 10:21:59 +0900

    mouse

---
+++ 05-mouse/js/cursor.js
@@ -0,0 +1,31 @@
+$(function () {
+    //대상을 변수에 저장
+    const $window = $(window);
+    const $cursor = $(".cursor");
+
+    // 마우스가 움직일 때
+    $window.on("mousemove", function (e) {
+        console.log(e);
+
+        // 마우스 x, y 좌표값을 받아서
+        const mouseX = e.pageX;
+        const mouseY = e.pageY;
+
+        // 가짜 마우스의 left, top 값으로 적용
+        $cursor.add(".circle").css({
+            left: mouseX,
+            top: mouseY,
+        });
+    });
+
+    //     $window.on("mousedown", function () {
+    //         $cursor.addClass("click");
+    //     });
+    //     $window.on("mouseup", function () {
+    //         $cursor.removeClass("click");
+    //     });
+
+    // 단축 코드
+    $window.on("mousedown", () => $cursor.addClass("click"));
+    $window.on("mouseup", () => $cursor.removeClass("click"));
+});
